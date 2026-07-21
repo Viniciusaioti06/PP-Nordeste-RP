7518a837e60b0ea597471ec9b5dd6e0673b438c1
Vinicius Rodrigues <vrsponte@gmail.com>
2026-07-21 08:15:49 -0300

    Corrigir URL Supabase

--- supabase/config.js
+++ supabase/config.js
@@ -3,7 +3,7 @@
  * Nunca coloque a service_role neste arquivo.
  */
 
-window.SUPABASE_URL = "https://yfsdexvuzhjcwwnwxyiy.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://yfsdexvuzhjcwwnwxyiy.supabase.co";
 
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlmc2RleHZ1emhqY3d3bnd4eWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MDI1NTMsImV4cCI6MjEwMDA3ODU1M30.e3yIfv_d-5ThLbmMyigAtIemcuYE0ivX6QL5Ty1RqZ0";
 
